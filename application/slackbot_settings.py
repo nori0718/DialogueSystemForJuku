0b6093ff41cc6b15b3012da3e653df1b43c191eb
Norisada Masui <masui@Norisada-no-MacBook-Pro.local>
2016-09-01 14:11:55 +0900

    remove k

--- application/slackbot_settings.py
+++ application/slackbot_settings.py
@@ -4,7 +4,7 @@ import os
 
 API_TOKEN = os.environ.get('SLACK_API_KEY', '')
  
-default_reply = "スイマセン。其ノ言葉ワカリマセン"
+default_reply = "すいません。その言葉わかりません"
 
 PLUGINS = [
     'plugins',
--- dialogue_system/backend/apis/docomo_dialogue.py
+++ dialogue_system/backend/apis/docomo_dialogue.py
@@ -13,5 +13,10 @@ class DocomoDialogAPI(object):
     def reply(self, text):
         response = self.__client.send(utt=text, apiname='Dialogue')
         utt = response['utt']
-
         return utt
+
+class DocomoKnowledge(object):
+    def __init__(self, api_key=None):
+        api_key = os.environ.get('DOCOMO_DIALOGUE_API_KEY', api_key)
+        self.__client = Client(apikey=api_key)
+        
