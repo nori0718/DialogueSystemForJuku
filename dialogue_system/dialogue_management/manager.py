5f5b34bd0869451d67fd1b65e84b2961f29aaf9b
Norisada Masui <masui@Norisada-no-MacBook-Pro.local>
2016-09-02 09:52:52 +0900

    repair

--- dialogue_system/dialogue_management/manager.py
+++ dialogue_system/dialogue_management/manager.py
@@ -28,6 +28,7 @@ class DialogueManager(object):
             reply = api.reply(dialogue_act['utt'])
             sys_act['sys_act_type'] = 'CHAT'
             sys_act['utt'] = reply
+            sys_act['sys_act_type'] = 'REQUEST_NAME'
         else:
             self.dialogue_state.clear()
 
